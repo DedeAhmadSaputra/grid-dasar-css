d484a7323ce38a9298399b24090365674ef396f1
DedeAhmadSaputra <saputradedeahmad@gmail.com>
2022-05-30 16:04:28 +0700

    gridcss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="logo">
+            <div className="item">logo</div>
+            <div className="item">Cisolok Store</div>
+            <div className="item">Jl.Cisolok 2022</div>
+            </div>
+      <div className="header">Header</div>
+      <div className="nav">Nav</div>
+      <div className="content">Content</div>
+      <div className="footer">Footer</div>
+      </div>
   );
 }
 
